c85a822dc37017f2f54c14366b4a19da6a0b69d2
ming yang <1638347567@qq.com>
2018-05-07 20:55:37 +0800

    change

--- src/pages/home/Header.vue
+++ src/pages/home/Header.vue
@@ -7,13 +7,12 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Header'
     }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  .hello{background: red}
+
 </style>
---
+++ src/pages/home/Home.vue
@@ -0,0 +1,24 @@
+<template>
+ <div class="hello">{{msg}}</div>
+ <index-header/>
+</template>
+
+<script>
+import Header from "./header.vue";	
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    },
+    components:{
+    	"index-header" : header
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .hello{background: red}
+</style>
